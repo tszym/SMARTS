d6585705eba5851a1ee798df56cde6880081c6cc
Thomas Szymanski <develop@tszymanski.com>
2021-11-27 16:46:09 +0100

    Use maven and its file layout

---
+++ src/test/java/edu/unimelb/smarts/processor/SimulatorTest.java
@@ -0,0 +1,20 @@
+package edu.unimelb.smarts.processor;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SimulatorTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
